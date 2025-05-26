88feaba97fda0dbd2023883adc981b2b710b78f2
mhuzaifa360 <mhuzaifa3128352877@gmail.com>
2025-05-26 23:02:27 +0500

    add another js file just empty, import all players pics again , create extra file for every team squad

---
+++ app.js
@@ -0,0 +1,29 @@
+
+
+// ?  Change picture of psl-winners section to caption of team on just hover
+
+// import picture of card
+// var isl_pic = document.getElementById("islamabad-pic").src;
+// var psh_pic = document.getElementById("peshawar-pic");
+// var que_pic = document.getElementById("quetta-pic");
+// var lhr_pic = document.getElementById("lahore-pic");
+// var krch_pic = document.getElementById("karachi-pic");
+// var mul_pic = document.getElementById("multan-pic");
+
+// import card
+// var isl_card = document.getElementById("islamabad-card");
+// var psh_card = document.getElementById("peshawar-card");
+// var que_card = document.getElementById("quetta-card");
+// var lhr_card = document.getElementById("lahore-card");
+// var krch_card = document.getElementById("karachi-card");
+// var mul_card = document.getElementById("multan-card");
+
+// change image on mouse enter
+// isl_card.addEventListener('mouseenter', () =>{
+//     isl_pic = './images/players/Shahdab-Khan.jpeg'
+// })
+
+// revert image on mouse leave
+// isl_card.addEventListener('mouseleave', ()=>{
+//    './images/players/Shahdab-Khan.jpeg' = isl_pic
+// })
